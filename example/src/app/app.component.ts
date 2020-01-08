91e38962cebaa718017e1ed7d09f48e41f13151d
wfsovereign <wfsovereign@outlook.cm>
2020-01-08 14:23:19 +0800

    doc: #8 update example project for selectable parameter

--- example/src/app/app.component.ts
+++ example/src/app/app.component.ts
@@ -40,105 +40,107 @@ const option = {
   container: 'jsmind_container',
   theme: 'normal',
   editable: true,
+  selectable: false,
   depth: 4,
   hierarchyRule: HIERARCHY_RULES,
   enableDraggable: true,
 };
 
 const mind = {
-  "format": "nodeTree",
-  "data": {
-    "id": 43,
-    "topic": "xx车行",
-    "selectedType": false,
-    "backgroundColor": "#7EC6E1",
-    "children": [
+  'format': 'nodeTree',
+  'data': {
+    'id': 43,
+    'topic': 'xx车行',
+    'selectedType': false,
+    'backgroundColor': '#7EC6E1',
+    'children': [
       {
-        "id": 80,
-        "color": "#fff",
-        "topic": "show room",
-        "direction": "right",
-        "selectedType": "销售经理",
-        "backgroundColor": "#616161",
-        "children": []
+        'id': 80,
+        'color': '#fff',
+        'topic': 'show room',
+        'direction': 'right',
+        'selectedType': '销售经理',
+        'backgroundColor': '#616161',
+        'children': []
       },
       {
-        "id": 44,
-        "color": "#fff",
-        "topic": "销售经理",
-        "direction": "right",
-        "selectedType": "销售经理",
-        "backgroundColor": "#616161",
-        "children": [
+        'id': 44,
+        'color': '#fff',
+        'topic': '销售经理',
+        'direction': 'right',
+        'selectedType': '销售经理',
+        'backgroundColor': '#616161',
+        'children': [
           {
-            "id": 46,
-            "color": "#fff",
-            "topic": "展厅经理",
-            "direction": "right",
-            "selectedType": "展厅",
-            "backgroundColor": "#989898",
-            "children": [
+            'id': 46,
+            'color': '#fff',
+            'topic': '展厅经理',
+            'direction': 'right',
+            'selectedType': '展厅',
+            'backgroundColor': '#989898',
+            'children': [
               {
-                "id": 49,
-                "color": "#fff",
-                "topic": "销售小组C",
-                "direction": "right",
-                "selectedType": "销售小组",
-                "backgroundColor": "#C6C6C6",
-                "children": []
+                'id': 49,
+                'color': '#fff',
+                'topic': '销售小组C',
+                'direction': 'right',
+                'selectedType': '销售小组',
+                'backgroundColor': '#C6C6C6',
+                'children': []
               },
               {
-                "id": 51,
-                "color": "#fff",
-                "topic": "AMG销售",
-                "direction": "right",
-                "selectedType": "销售小组",
-                "backgroundColor": "#C6C6C6",
-                "children": []
+                'id': 51,
+                'color': '#fff',
+                'topic': 'AMG销售',
+                'direction': 'right',
+                'selectedType': '销售小组',
+                'backgroundColor': '#C6C6C6',
+                'children': []
               },
               {
-                "id": 47,
-                "color": "#fff",
-                "topic": "销售小组A",
-                "direction": "right",
-                "selectedType": "销售小组",
-                "backgroundColor": "#C6C6C6",
-                "children": []
+                'id': 47,
+                'color': '#fff',
+                'topic': '销售小组A',
+                'direction': 'right',
+                'selectedType': '销售小组',
+                'backgroundColor': '#C6C6C6',
+                'children': []
               },
               {
-                "id": 48,
-                "color": "#fff",
-                "topic": "销售小组B",
-                "direction": "right",
-                "selectedType": "销售小组",
-                "backgroundColor": "#C6C6C6",
-                "children": []
+                'id': 48,
+                'color': '#fff',
+                'topic': '销售小组B',
+                'direction': 'right',
+                'selectedType': '销售小组',
+                'backgroundColor': '#C6C6C6',
+                'children': []
               },
               {
-                "id": 50,
-                "color": "#fff",
-                "topic": "销售小组D",
-                "direction": "right",
-                "selectedType": "销售小组",
-                "backgroundColor": "#C6C6C6",
-                "children": []
+                'id': 50,
+                'color': '#fff',
+                'topic': '销售小组D',
+                'direction': 'right',
+                'selectedType': '销售小组',
+                'backgroundColor': '#C6C6C6',
+                'children': []
               }
             ]
           }
         ]
       },
       {
-        "id": 45,
-        "color": "#fff",
-        "topic": "Smart经理",
-        "direction": "right",
-        "selectedType": "销售经理",
-        "backgroundColor": "#616161",
-        "children": []
+        'id': 45,
+        'color': '#fff',
+        'topic': 'Smart经理',
+        'direction': 'right',
+        'selectedType': '销售经理',
+        'backgroundColor': '#616161',
+        'children': []
       }
     ]
   }
 };
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
